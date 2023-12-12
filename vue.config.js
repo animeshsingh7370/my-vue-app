91ca818338d51aa106dd8b20f5dea3a56ec7276f
animeshsingh7370 <animeshsingh161@gmail.com>
2023-12-12 19:27:01 +0530

    final commit

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
 module.exports = {
   devServer: {
     proxy: {
-      '/api': {
-        target: 'https://sv6yuqrxwe.execute-api.us-east-2.amazonaws.com',
+      "/api": {
+        target: "https://sv6yuqrxwe.execute-api.us-east-2.amazonaws.com",
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  }
-}
+          "^/api": "",
+        },
+      },
+    },
+  },
+};
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/my-vue-app/" : "/",
+};
